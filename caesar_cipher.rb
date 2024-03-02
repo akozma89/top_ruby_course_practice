e6779a6c04e34dabf6401cdd654d6f2009018770
Arpad Kozma <arpadkozma89@gmail.com>
2024-03-02 20:07:47 +0100

    Add Caesar Cipher implementation
    A new file, caesar_cipher.rb, has been added which implements the Caesar Cipher encryption technique. The code includes functions to locate the ciphered character and perform the encryption on an input string, supporting both uppercase and lowercase alphabet.

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+SMALL_ALPHABET = ("a".."z").to_a
+LARGE_ALPHABET = ("A".."Z").to_a
+
+def find_ciphered_character(character, rotation)
+  if SMALL_ALPHABET.include?(character)
+    SMALL_ALPHABET.rotate(rotation)[SMALL_ALPHABET.index(character)]
+  elsif LARGE_ALPHABET.include?(character)
+    LARGE_ALPHABET.rotate(rotation)[LARGE_ALPHABET.index(character)]
+  else
+    character
+  end
+end
+
+def perform_caesar_cipher(input_string = "", rotation = 5)
+  input_string.chars.map { |character| find_ciphered_character(character, rotation) }.join
+end
+
+puts "What do you want to convert?"
+print perform_caesar_cipher(gets)
