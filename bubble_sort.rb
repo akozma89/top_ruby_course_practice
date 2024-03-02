62e87c3a240a267b9b30ce658162cada977d5097
Arpad Kozma <arpadkozma89@gmail.com>
2024-03-02 23:18:49 +0100

    Update algorithm collection with bubble sort function
    This addition includes a new Ruby file implementing the bubble sort algorithm. It sorts an input array from least to greatest and will be reusable for future array sorting needs.

---
+++ bubble_sort.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def bubble_sort(input_array = [], remaining = input_array.length)
+  return input_array if remaining == 1
+
+  changed = false
+
+  input_array.each_with_index do |item, index|
+    if index < input_array.size - 1 && item > input_array[index + 1]
+      input_array[index], input_array[index + 1] = input_array[index + 1], input_array[index]
+      changed = true
+    end
+  end
+
+  return input_array unless changed
+
+  bubble_sort(input_array, remaining - 1)
+end
+
+puts bubble_sort([4,3,78,2,0,2]).join(",")
+
+# bubble_sort([4,3,78,2,0,2])
+# [0,2,2,3,4,78]
